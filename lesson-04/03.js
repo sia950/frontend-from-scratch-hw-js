f76b3354ae59bd93c7794019cfb1bfb7605a77bb
sia950 <kim950136@gmail.com>
2025-07-02 15:04:05 +0300

    aaaaaaaaaaa;

--- lesson-04/03.js
+++ lesson-04/03.js
@@ -26,5 +26,6 @@ function findCommonElements(firstArray, secondArray) {
     }
     return newArray
 }
-// const hhhhh = findCommonElements([1, 2, 3], [2, 3, 4])
+// const hhhhh = 
+findCommonElements([1, 2, 3], [2, 3, 4])
 // console.log(hhhhh)
